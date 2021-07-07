07134d7c2d429b081eda0320b196f8e2eb353775
Oleksii Kolotenko <a1708kolot@gmail.com>
2021-07-07 21:53:49 +0300

    added security by env

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authRouter = require("./authRouter");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const app = express();
 
@@ -10,10 +11,10 @@ app.use(express.json());
 app.use("/auth", authRouter);
 const start = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://oleksiy:Resistwater1@cluster0.mo392.mongodb.net/Cluster0?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
   } catch (e) {
     console.log(e);
