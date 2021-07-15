890debd0c54180372a4d35fd52a5e97f9c5520ec
Oleksii Kolotenko <a1708kolot@gmail.com>
2021-07-15 23:36:49 +0300

    edit works

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const authRouter = require("./authRouter");
+const mainRouter = require("./mainRouter");
 const PORT = process.env.PORT;
 const cors = require("cors");
 const app = express();
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
-app.use("/auth", authRouter);
+app.use("/auth", mainRouter);
 const start = async () => {
   try {
     await mongoose.connect(process.env.MONGO, {
